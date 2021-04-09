6e4df6edc9e8f2dc1a8c82bd2f637ed3c09c15aa
Jonas Kohls <kohlsjw3656@uwec.edu>
2021-04-09 18:46:41 -0500

    Added redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import reducer from './reducer';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./reducer";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 export default store;
